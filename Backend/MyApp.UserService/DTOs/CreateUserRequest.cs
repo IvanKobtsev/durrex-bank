59b3a698f90c8cc11342b5c22fa25c09f2f01b1f
Ilya_Uymanov <ninjaxxxflame@gmail.com>
2026-02-25 22:14:03 +0700

    Swagger connected

--- Backend/MyApp.UserService/DTOs/CreateUserRequest.cs
+++ Backend/MyApp.UserService/DTOs/CreateUserRequest.cs
@@ -11,6 +11,7 @@ public record CreateUserRequest(
     [Required] [MinLength(3)] [MaxLength(64)] string Username,
     [Required] [MinLength(6)] string Password,
     Role Role
+// I guess we don't need to specify IsBlocked, but if we want to create user being blocked, let me know
 );
 
 // TODO: add validation attributes for password

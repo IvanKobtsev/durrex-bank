1e8c258d8c9124771b06909758831160d06eb256
Ilya Uimanov <ninjaxxxflame@gmail.com>
2026-03-22 17:54:50 +0700

    Restore MyApp.SettingsService on main

---
+++ Backend/MyApp.SettingsService/Models/UserSettings.cs
@@ -0,0 +1,9 @@
+namespace MyApp.SettingsService.Models;
+
+public class UserSettings
+{
+    public int UserId { get; set; }
+    public string Theme { get; set; } = "light";
+    public int[] HiddenAccountIds { get; set; } = [];
+    public DateTimeOffset UpdatedAt { get; set; }
+}

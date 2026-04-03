8c3adcc96b8261ddd0da4bb7c2122afc92129c64
Ivan Kobtsev <ivan.kobtsev@mcc-soft.de>
2026-04-03 23:34:46 +0700

    Updated some more destinations

--- Backend/MyApp.Gateway/Program.cs
+++ Backend/MyApp.Gateway/Program.cs
@@ -89,7 +89,7 @@ builder.Services.AddCors(options =>
         policy =>
         {
             policy
-                .WithOrigins("http://localhost:5173", "http://localhost:5174")
+                .WithOrigins("https://swagor-time.ru")
                 .AllowAnyHeader()
                 .AllowAnyMethod()
                 .AllowCredentials();
